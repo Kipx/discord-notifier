6f4c11e8bf1e5f81d611f47c662e1bcec6b67171
Kipx <cwhitney36@gmail.com>
2021-10-16 08:21:41 -0400

    Base Configuration

--- src/main/java/com/discordnotifier/DiscordNotifierPlugin.java
+++ src/main/java/com/discordnotifier/DiscordNotifierPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.discordnotifier;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Discord Notifier"
 )
-public class ExamplePlugin extends Plugin
+public class DiscordNotifierPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private DiscordNotifierConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	DiscordNotifierConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(DiscordNotifierConfig.class);
 	}
 }
--- src/test/java/com/discordnotifier/DiscordNotifierPluginTest.java
+++ src/test/java/com/discordnotifier/DiscordNotifierPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.discordnotifier;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DiscordNotifierPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DiscordNotifierPlugin.class);
 		RuneLite.main(args);
 	}
 }
