e7c2ad9da2f955fa3d688c60f4b451654480ca41
Kipx <cwhitney36@gmail.com>
2021-10-16 17:00:12 -0400

    Added Config Settings

--- src/main/java/com/discordnotifier/DiscordNotifierConfig.java
+++ src/main/java/com/discordnotifier/DiscordNotifierConfig.java
@@ -3,17 +3,78 @@ package com.discordnotifier;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("example")
 public interface DiscordNotifierConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+	@ConfigSection(
+			position = 1,
+			name = "Webhook Settings",
+			description = "Settings pertaining to the webhook"
 	)
-	default String greeting()
-	{
-		return "Hello";
+	String webhookSettings = "webhookSettings";
+
+	@ConfigItem(
+			keyName = "webhookurl",
+			name = "Webhook URL(s)",
+			description = "Discord Webhook URLs, each on their own line",
+			section = webhookSettings,
+			position = 1
+	)
+	default String webhookURL(){
+		return "";
+	}
+
+	@ConfigSection(
+			position = 2,
+			name = "Quest Settings",
+			description = "Settings pertaining to the quest notifications"
+	)
+	String questSettings = "questSettings";
+
+	@ConfigItem(
+			keyName = "questenable",
+			name = "Enable Quest Notifications",
+			description = "Whether or not to send quest notifications",
+			section = questSettings,
+			position = 1
+	)
+	default boolean questEnable(){
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "questsendscreenshot",
+			name = "Send Screenshot",
+			description = "Whether or not to send a screenshot with the notification",
+			section = questSettings,
+			position = 2
+	)
+	default boolean questSendScreenshot(){
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "questtypesenabled",
+			name = "Enabled Quest Types",
+			description = "Which quest types to send notifications for (separated by commas); the types are as follows: " +
+					"f2p (Free to Play), Member, Novice, Intermediate, Experienced, Master, Grandmaster, Special, Miniquest",
+			section = questSettings,
+			position = 3
+	)
+	default String questTypesEnabled(){
+		return "f2p,member,novice,intermediate,experienced,master,grandmaster,special,miniquest";
+	}
+
+	@ConfigItem(
+			keyName = "questpointminimum",
+			name = "Quest Point Minimum",
+			description = "Minimum amount of quest points necessary to send a notification",
+			section = questSettings,
+			position = 4
+	)
+	default int questPointMinimum(){
+		return 1;
 	}
 }
